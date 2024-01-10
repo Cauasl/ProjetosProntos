b9d013a1508a5a122a59d3b5396763f0b8c80a85
Cauasl <cacasaraivalopes@gmail.com>
2024-01-10 13:50:28 -0300

    Atualizando Portifolio

--- Portifolio/interatividade.js
+++ Portifolio/interatividade.js
@@ -9,13 +9,16 @@ for(var i=0; i < quantINPUT.length; i++) {
 	if(sim_nao) {
 		img[val].style.display = "none";
 		iframe[val].style.display = "block";
+		quantINPUT[val].setAttribute("value", "< Ver imagem do site");
 		sim_nao = !sim_nao;		
 		
 	}else {
 		img[val].style.display = "block";
 		iframe[val].style.display = "none";
+		quantINPUT[val].setAttribute("value", "Ver pré visualização >");
 		sim_nao = !sim_nao;
 	}
+	
 });
 	}(i));
 }
