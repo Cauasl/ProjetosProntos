94f299427300148f01dded712695310f6a48f7a3
Cauã S. Lopes <154275368+Cauasl@users.noreply.github.com>
2024-01-29 11:33:28 -0300

    a

--- Portifolio/interatividade.js
+++ Portifolio/interatividade.js
@@ -1,30 +1,3 @@
-//Sistema das trocas de imagens para iframe e visse e versa.
-let quantINPUT = document.getElementsByName('bt');
-let iframe = document.getElementsByTagName('iframe');
-let img = document.getElementsByTagName('img');
-
-for(var i=0; i < quantINPUT.length; i++) {
-	let sim_nao = true;
-	(function (val) {
-		quantINPUT[val].addEventListener("click", function() {
-	if(sim_nao) {
-		img[val].style.display = "none";
-		iframe[val].style.display = "block";
-		quantINPUT[val].setAttribute("value", "< Ver imagem do site");
-		sim_nao = !sim_nao;		
-		
-	}else {
-		img[val].style.display = "block";
-		iframe[val].style.display = "none";
-		quantINPUT[val].setAttribute("value", "Ver pré visualização >");
-		sim_nao = !sim_nao;
-	}
-	
-});
-	}(i));
-}
-
-
 //Sistema da mostra de contato.
 function MostrarContato() {
 	let divContato = document.getElementById('divContato');
