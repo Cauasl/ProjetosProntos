a580180eb4cb58fd096e2e3282bdfa7de30ee810
Cauã S. Lopes <154275368+Cauasl@users.noreply.github.com>
2024-01-29 18:55:20 -0300

    Colocando outro projeto

---
+++ SiteLoja/code/script.js
@@ -0,0 +1,34 @@
+
+let aviso = document.getElementById('aviso');
+
+function Verificar() {
+	let email = document.getElementById('iemail').value;
+	let RegEx = /([a-zA-Z|0-9]{2,})[@]([a-zA-Z|0-9]{2,})(\.[a-z]{2,})(\.[a-z]{2})?/g;
+	
+	if(RegEx.test(email)) {
+		window.location.href = 'index.html';
+		aviso.innerText = '';
+	}else {
+		aviso.innerText = 'Email inválido!';
+	}
+
+	let telnum = document.getElementById('inumero');
+	if(telnum != null || telnum != undefined) {
+		let RegExNum = /([0-9]{2})\s?([0-9])?([0-9]){4}\-?([0-9]{4})/g;
+		let numero = telnum.value;
+		if(RegExNum.test(numero)) {
+			alert('Numero de boa');
+		}
+	}
+}
+
+function MostrarSenha() {
+	let senha = document.getElementById('isenha');
+	let mostar = document.getElementById('msenha').checked;
+	senha.type = mostar? 'text':'password';
+
+	let consenha = document.getElementById('icsenha');
+	if(consenha != null || consenha != undefined) {
+		consenha.type = mostar? 'text':'password';
+	}
+}
