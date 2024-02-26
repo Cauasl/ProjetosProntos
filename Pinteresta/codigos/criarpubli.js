7a60b30ff82d3b8bda25e5eed1e948a74ecb9be9
Cauã S. Lopes <154275368+Cauasl@users.noreply.github.com>
2024-02-25 22:27:11 -0300

    Começando/Criando Super Calculadoa

--- Pinteresta/codigos/criarpubli.js
+++
@@ -1,8 +0,0 @@
-import { CriarMandarPublicacao } from './script_index_modulo.js';
-
-document.getElementById('botao').addEventListener('click', function() {
-   let url = 'https://jsonplaceholder.typicode.com/photos';
-   let arquivo = document.getElementById('imagemcarregada').value;
-   let titulo = document.getElementById('titulo-imagem').value;
-   CriarMandarPublicacao(arquivo, titulo, url);
-});
